7d086e6ae9fe4d17746b9679e54a00caaebc4c93
alethal <git@shawneee.com>
2021-07-12 04:03:47 -0700

    build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/spiffy/Documents/clicketyappy/gatsby-one/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/spiffy/Documents/clicketyappy/gatsby-one/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/spiffy/Documents/clicketyappy/gatsby-one/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/Users/spiffy/Documents/clicketyappy/gatsby-one/src/pages/page-2.js")),
