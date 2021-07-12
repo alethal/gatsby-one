1bcd2abf9642410b7ae4d6374a9f29d93419e97c
alethal <git@shawneee.com>
2021-07-12 04:10:23 -0700

    tiny changes

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,8 @@ import Seo from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>2nd page</h1>
+    <p>Blah.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
